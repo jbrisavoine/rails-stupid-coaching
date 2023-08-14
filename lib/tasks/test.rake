82c57e68907a32418b4d8716dd18e206b1e86fd0
Jonathan Brisavoine <john.brv@gmail.com>
2023-08-14 14:28:36 +0200

    Stupid Coaching

---
+++ lib/tasks/test.rake
@@ -0,0 +1,8 @@
+﻿namespace :test do
+  namespace :system do
+    task :with, [:driver] => :environment do |task, args|
+      ENV["DRIVER"] = args[:driver]
+      Rake::Task["test:system"].invoke
+    end
+  end
+end
--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,13 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  # driven_by :headless_chrome # Update this line
+  DRIVER = if ENV["DRIVER"]
+    ENV["DRIVER"].to_sym
+  else
+    :headless_chrome
+  end
+
+  driven_by :selenium, using: DRIVER, screen_size: [1400, 1400]
 end
